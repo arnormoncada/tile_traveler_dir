8d720fbe25a7e520ed06b9a14289071157bc400e
Birkir Ö. Thorsteinsson <birkir.thorsteinsson@gmail.com>
2020-09-17 10:21:52 +0000

    Beinagrind

---
+++ tile_traveller.py
@@ -0,0 +1,10 @@
+print("You can travel: (N)orth")
+directions = input("Direction: ")
+
+if spots == 11:
+        haekkun = "n"
+        the_input=print("You can travel:",if spots == 11:
+        haekkun = "n"
+        the_input=print("You can travel:", haekkun)
+        if directions =="n":
+        
