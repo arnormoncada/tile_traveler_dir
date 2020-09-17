0491b983abce7f01c8145d4c6b8d49f14bf7ff40
Stefán Orri <stefan@Stefans-MacBook-Pro.local>
2020-09-17 12:23:27 +0000

    frethaus

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,70 +1,64 @@
-print("You can travel: (N)orth")
-directions = input("Direction: ")
-
+valid=""
 N = "(N)orth"
 S = "(S)outh"
 E = "(E)ast"
 W = "(W)est"
-
-x_pos , y_pos = 1,1
-start_point = x_pos ,y_pos
-
+x=1
+y=1
 def update(x):
     return x+1
-def fun(direct, x, y):
-    if direct == N:
+def stadsetning(direct, x, y): # Benedikt hjálpaði okkur með þetta
+    if direct == "N":
         return x, y +1
-    if direct == S:
+    if direct == "S":
         return x, y-1
-    if direct == E:
+    if direct == "E":
         return x+1,y
-    if direct == W:
+    if direct == "W":
         return x-1, y
-
-
-
-
-"""while True:
-    if (x_pos == 1) and (y_pos == 1):
-        print(f"You can travel: {N}.")
-        direction = str(input("Direction: "))
-        while True:
-            if direction.lower() == 'n':
-                y_pos +=1
-                break
-            else: 
-                print("Not a valid direction")
-                direction = str(input("Direction: "))
-    if (x_pos == 1) and (y_pos == 2):
-        print(f"You can travel: {N} or {E} or {S}.")
-        direction = str(input("Direction: "))
-        while True:
-            if direction.lower() in ('n', 's', 'e'):
-                if direction.lower() == 'n':
-                    y_pos += 1
-                    break
-                elif direction.lower() == 's':
-                    y_pos -= 1
-                    break
-                elif direction.lower() == 'e':
-                    x_pos += 1
-                    break
-            else: 
-                print("Not a valid direction!")
-                direction = str(input("Direction: "))
-    if (x_pos == 1) and (y_pos == 3):
-        print(f"You can travel: {E} or {S}.")
-        direction = str(input("Direction: "))
-        while True:
-            if direction.lower() in ('s', 'e'):
-                if direction.lower() == 's':
-                    y_pos -= 1
-                    break
-                elif direction.lower() == 'e':
-                    x_pos += 1
-                    break
-            else:
-                print("Not a valid direction!")
-                direction = str(input("Direction: "))"""
-
-        
+while True:
+    if x == 1 and y == 1:
+        print("You can travel: (N)orth")
+        directions = input("Direction: ")
+        if directions != "N":
+            print("Not a valid direction!")
+        else:
+            x,y = stadsetning(directions,x,y)
+    elif x==1 and y == 2:
+        print("You can travel: (N)orth")
+        print("You can travel: (S)outh")
+        print("You can travel: (E)ast)")
+        directions = input("Direction: ")
+        if directions != "N" or directions != "S" or directions != "E":
+            print("Not a valid direction!")
+        else:
+            x,y = stadsetning(directions,x,y)
+    elif x==1 and y == 3:
+        print("You can travel: (S)outh")
+        print("You can travel: (E)ast)")
+        directions = input("Direction: ")
+        if directions != "S" or "E":
+            print("Not a valid direction!")
+        else:
+            x,y = stadsetning(directions,x,y)
+    elif x==2 and y == 1:
+        print("You can travel: (N)orth")
+        directions = input("Direction: ")
+    elif x==2 and y==2:
+        print("You can travel: (S)outh")
+        print("You can travel: (W)est)")
+        directions = input("Direction: ")
+    elif x==2 and y==3:
+        print("You can travel: (E)ast")
+        print("You can travel: (W)est)")
+        directions = input("Direction: ")
+    elif x==3 and y==3:
+        print("You can travel: (S)outh")
+        print("You can travel: (W)est)")
+        directions = input("Direction: ")
+    elif x==3 and y==2:
+        print("You can travel: (S)outh")
+        print("You can travel: (N)orth)")
+        directions = input("Direction: ")
+else: 
+    print("Victory!")
